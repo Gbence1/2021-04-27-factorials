bc9807bc6ed444a6cd9a3a3c19539f141f8f978a
Bence Gallai <bencegallai460@gmail.com>
2021-04-30 23:44:38 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let n = 0;
+let counter = counterFunc();
+
+document.createElement("table");
+document.querySelector("table").appendChild("thead");
+
+
+function counterFunc() {
+    for (let i = 0; i <= 20; i++) {
+
+    }
+}
